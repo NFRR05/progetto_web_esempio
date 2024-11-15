15b677315e1edea40fe4c8762e5f4fb4c784f7bf
nfrr05 <riccardo.remec@itsincom.it>
2024-11-15 13:00:11 +0100

    aggiunto un bottone

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-/* eslint-disable no-console */
+
+function Assistenza(): string | null
+{
+    let valore : string | null;
+    valore = prompt();
+    if(valore == null || valore == "")
+    {
+        alert("Hai inserito un valore non valido");
+    }
+    return valore;
+}
+
+//---------------------------------------------------------
 
 function main(): void
-{
-    console.log("Hello, world!");
+{ 
+    alert("Il tuo valore e': " + Assistenza());
+    
 }
+
+//--------------------------------------------------------
 
 main();
